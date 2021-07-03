2bab4f26b8bdf8bdf872cc8b66ba650f9b262e8d
whirledsol <astro491@gmail.com>
2021-07-02 20:48:45 -0400

    titlebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import { Container } from '@material-ui/core';
 import Title from './components/Title';
 
+
+
+
+
+
 const App = () => {
   return (
     <>
       <Title>gitpile</Title>
-      <Container>
-        Content
-      </Container>
+
     </>
   );
 }
--- client/src/components/Title.js
+++ client/src/components/Title.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import { AppBar, Toolbar, Typography, IconButton, Icon } from '@material-ui/core';
 
 const Title = (props) => {
   return (
-    <Typography component="h2" variant="h6" color="primary" gutterBottom>
-      {props.children}
-    </Typography>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <Icon className="fa fa-bars" />
+        </IconButton>
+        <Typography variant="h6" >
+          gitpile
+        </Typography>
+        <IconButton color="inherit"><Icon className="fa fa-edit" /></IconButton>
+      </Toolbar>
+    </AppBar>
   );
 };
 
