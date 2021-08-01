910ef6692859f743944600eca4b5b05df1bd1197
whirledsol <astro491@gmail.com>
2021-08-01 15:29:43 -0400

    restructuring to prepare for electron

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,3 @@
-const app = require('./app');
+const server = require('./server');
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
-  /* eslint-enable no-console */
-});
+server();
---
+++ server/src/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const path = require('path');
+
+
+
+const server = _=>{
+	require('dotenv').config();
+	const {API_VERSION} = process.env;
+
+
+	const middlewares = require('./middlewares');
+
+	const app = express();
+
+	global.appRoot = path.resolve(__dirname);
+	app.use(morgan('dev'));
+	app.use(helmet());
+	app.use(cors());
+	app.use(express.json());
+
+	const route_api = require('./api');
+	const route_git = require('./api/git');
+	const route_interface = require('./api/interface');
+	app.use(`/api/${API_VERSION}`, route_api);
+	app.use(`/api/${API_VERSION}/git`, route_git);
+	app.use(`/api/${API_VERSION}/interface`, route_interface);
+
+	app.use(middlewares.notFound);
+	app.use(middlewares.errorHandler);
+
+
+	const port = process.env.PORT || 5000;
+	app.listen(port, () => {
+		/* eslint-disable no-console */
+		console.log(`Listening: http://localhost:${port}`);
+		/* eslint-enable no-console */
+	});
+
+	return app;
+};
+
+module.exports = server;
